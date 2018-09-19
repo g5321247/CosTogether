05d254fd738bd7c1c85f2ee3689f0e71dae025cc
GeorgeLIu <g5321247@gmail.com>
2018-09-19 11:32:34 +0800

    Add gitignore and new project

---
+++ CosTogether/CosTogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CosTogether
+//
+//  Created by George Liu on 2018/9/19.
+//  Copyright © 2018 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
