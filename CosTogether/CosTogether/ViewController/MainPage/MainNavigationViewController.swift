ce2594da09889b23a02eb37537aaa174a7702ece
GeorgeLIu <g5321247@gmail.com>
2018-09-25 11:52:42 +0800

    add navigation controller to main page

---
+++ CosTogether/CosTogether/ViewController/MainPage/MainNavigationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainNavigationViewController.swift
+//  CosTogether
+//
+//  Created by George Liu on 2018/9/25.
+//  Copyright © 2018 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor = UIColor.white
+    }
+    
+
+}
