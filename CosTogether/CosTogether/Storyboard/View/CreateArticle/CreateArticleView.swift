5a2fd21b552a1c6f4fac57ef28a6f50349ae4099
GeorgeLIu <g5321247@gmail.com>
2018-10-07 00:41:37 +0800

    set openGroup page

---
+++ CosTogether/CosTogether/Storyboard/View/CreateArticle/CreateArticleView.swift
@@ -0,0 +1,77 @@
+//
+//  CreateArticleView.swift
+//  CosTogether
+//
+//  Created by George Liu on 2018/10/6.
+//  Copyright © 2018 George Liu. All rights reserved.
+//
+
+import UIKit
+
+class CreateArticleView: UIView {
+
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var titleTxf: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        contentTextView.delegate = self
+        
+        setup()
+    }
+
+    private func setup() {
+        textSetup()
+    }
+    
+    private func textSetup() {
+        
+        contentTextView.cornerSetup(
+            cornerRadius: 4,
+            borderWidth: 1,
+            borderColor: UIColor.lightGray.cgColor
+        )
+        
+        contentTextView.text = "請輸入揪團詳細資訊內容..."
+        contentTextView.textColor = UIColor.lightGray
+
+        
+        titleTxf.cornerSetup(
+            cornerRadius: 4,
+            borderWidth: 1,
+            borderColor: UIColor.lightGray.cgColor
+        )
+        
+    }
+    
+}
+
+extension CreateArticleView: UITextViewDelegate {
+  
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        
+        if textView.textColor == UIColor.lightGray {
+            
+            textView.text = nil
+            textView.textColor = UIColor.black
+            
+        }
+        
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        if textView.text.isEmpty {
+            
+            textView.text = "請輸入揪團詳細資訊內容..."
+            textView.textColor = UIColor.lightGray
+            
+        }
+        
+    }
+
+}
+
+
+
