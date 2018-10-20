d12e9b2a32bce8c6e69281de8d34b8762c79548e
GeorgeLIu <g5321247@gmail.com>
2018-10-20 20:17:55 +0800

    製作隱私權頁面

--- CosTogether/CosTogether/ViewController/Agreement/AgreementViewController.swift
+++ CosTogether/CosTogether/ViewController/Agreement/AgreementViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import WebKit
+import SVProgressHUD
+import NotificationBannerSwift
 
 class AgreementViewController: UIViewController {
     
@@ -23,7 +25,7 @@ class AgreementViewController: UIViewController {
     private func setup() {
         
         webViewSetup()
-        
+        startingPage()
     }
     
     private func webViewSetup() {
@@ -33,8 +35,23 @@ class AgreementViewController: UIViewController {
         
     }
     
+    private func startingPage() {
+        
+        SVProgressHUD.show()
+        
+        guard let url = URL(string: "https://termsfeed.com/eula/ae69cb87067ad072279365009dece100") else {
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+
+    }
+    
     @IBAction func goToEULAWeb(_ sender: UIButton) {
         
+        SVProgressHUD.show()
+
         guard let url = URL(string: "https://termsfeed.com/eula/ae69cb87067ad072279365009dece100") else {
             return
         }
@@ -43,23 +60,41 @@ class AgreementViewController: UIViewController {
         webView.load(request)
         
     }
-
-    @IBAction func  goToTermOfServiceWeb(_ sender: UIButton) {
-        
-        
-    }
     
     @IBAction func goToPrivicyWeb(_ sender: UIButton) {
         
+        SVProgressHUD.show()
+        
+        guard let url = URL(string: "https://privacypolicies.com/privacy/view/71d86619bed81edc83c6c67895823dd2") else {
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
         
     }
-
-
+    
+    
+    @IBAction func backToLoginPage(_ sender: UIButton) {
+        
+        dismiss(animated: true)
+    }
+    
 }
 
 extension AgreementViewController: WKUIDelegate, WKNavigationDelegate {
     
     
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        SVProgressHUD.dismiss()
+    }
     
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        
+        BaseNotificationBanner.warningBanner(subtitle: "連線失敗，請確認網路連線狀況")
+        
+        SVProgressHUD.dismiss()
+    }
     
 }
